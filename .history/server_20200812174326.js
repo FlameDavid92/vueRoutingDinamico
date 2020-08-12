0087a6f23586c20573ee2aaee87da35dd73030ef
FlameDavid92 <davidefiguccia2020@outlook.it>
2020-08-12 17:47:55 +0200

    Routing dinamico

--- .history/server_20200812174326.js
+++ .history/server_20200812174326.js
@@ -5,6 +5,9 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.get('/', (req, res) => {
+    res.sendFile('dist/index.html');
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);
