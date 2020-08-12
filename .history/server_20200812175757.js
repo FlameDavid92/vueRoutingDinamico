02c46b2d37ab19cbe5a64f1e6b0efc835ea18b3a
FlameDavid92 <davidefiguccia2020@outlook.it>
2020-08-12 18:06:50 +0200

    Routing server ok

--- .history/server_20200812175757.js
+++ .history/server_20200812175757.js
@@ -1,13 +1,14 @@
+//server.js
 var express = require('express');
 const history = require('connect-history-api-fallback');
 var path = require('path');
 var serveStatic = require('serve-static');
-const app = express();
-app.use(history());
+app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.use(history());
 app.get('/', (req, res) => {
-  res.sendFile('dist/index.html');
+    res.sendFile('dist/index.html');
 });
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+console.log('server started ' + port);
