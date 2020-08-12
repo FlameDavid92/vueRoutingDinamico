84a10f677b72237aa58b9dedda57a1f139944bdb
FlameDavid92 <davidefiguccia2020@outlook.it>
2020-08-12 17:55:12 +0200

    Adding files.

--- .history/server_20200812175133.js
+++ .history/server_20200812175133.js
@@ -1,14 +1,11 @@
 //server.js
+
 var express = require('express');
 const history = require('connect-history-api-fallback');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-app.use(history());
-app.get('/', (req, res) => {
-    res.sendFile('dist/index.html');
-});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);
