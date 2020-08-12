856ae7501466f48a85e5bf50282f4f138cba64ae
FlameDavid92 <davidefiguccia2020@outlook.it>
2020-08-12 15:59:35 +0200

    Routing Dinamico con Vue e server Node

---
+++ .history/server_20200812154540.js
@@ -0,0 +1,17 @@
+//server.js
+const express = require('express');
+const history = require('connect-history-api-fallback');
+const app =express();
+
+app.use(history());
+
+var serveStatic = require('serve-static');
+app.use(serveStatic(__dirname + "/dist"));
+
+app.get('/', (req, res) => {
+  res.sendFile('dist/index.html');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+port);
